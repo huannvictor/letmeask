08913de5b0bed5f4e1b5df96a40e5098a0b06b5a
Huann Almeida <huannvictor@gmail.com>
2021-12-12 23:28:07 -0300

    created style, routes and navgation, autentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { Button } from "./components/Button";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import {Home} from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+
+
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <Router>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/rooms/new' element={<NewRoom />} />
+        </Routes>
+      </AuthContextProvider>
+    </Router>
   );
 }
 
