b7281ad5e843e696c3a1fdd861feec1c49b2518a
Huann Almeida <huannvictor@gmail.com>
2021-12-19 16:07:15 -0300

    funcionalidade curtir e descurtir

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,17 +1,21 @@
+import { ReactNode } from 'react'
+
 import './styles.scss'
 
 type QuestionProps = {
-  content: string,
+  content: string;
   author: {
-    name: string,
-    avatar: string,
-  }
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
 }
 
 export function Question(
   {
     content,
     author,
+    children,
   }: QuestionProps) {
     return (
       <div className="question">
@@ -21,7 +25,9 @@ export function Question(
             <img src={author.avatar} alt={author.name} />
             <span>{author.name}</span>
           </div>
-          <div></div>
+          <div>
+            { children }
+          </div>
         </footer>
       </div>
     )
